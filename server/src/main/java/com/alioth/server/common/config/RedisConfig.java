f490d60596779661941466f571c33671d238cc6d
GyeongNam <63902992+GyeongNam@users.noreply.github.com>
2024-03-25 12:11:47 +0900

    RDB, Redis 연결 완료 (#3)
    DB 연결 : localhost:3306 MariaDB
    Redis 연결 : localhost:6379 Redis

--- server/src/main/java/com/alioth/server/common/config/RedisConfig.java
+++ server/src/main/java/com/alioth/server/common/config/RedisConfig.java
@@ -1,5 +1,7 @@
 package com.alioth.server.common.config;
 
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -11,10 +13,10 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 public class RedisConfig {
 
-    //@Value("${spring.redis.host}")
+    @Value("${spring.data.redis.host}")
     private String redisHost;
 
-
+    @Value("${spring.data.redis.port}")
     private String redisPort;
 
 
@@ -26,7 +28,7 @@ public class RedisConfig {
         RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
         redisStandaloneConfiguration.setHostName(redisHost);
         redisStandaloneConfiguration.setPort(Integer.valueOf(redisPort));
-        redisStandaloneConfiguration.setPassword(redisPassword);
+        // redisStandaloneConfiguration.setPassword(redisPassword);
         LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory(redisStandaloneConfiguration);
 
         return lettuceConnectionFactory;
