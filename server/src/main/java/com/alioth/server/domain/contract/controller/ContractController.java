d281b387e613df499d415a2251303a353ebb16cd
Mr.Jo <142702766+hoonjo123@users.noreply.github.com>
2024-04-18 11:41:29 +0900

    Jh/board page (#64)
    * JH/Board코드 수정
    
    * [feat]: frontend 개발을 위한 contract, board 코드 수정

--- server/src/main/java/com/alioth/server/domain/contract/controller/ContractController.java
+++ server/src/main/java/com/alioth/server/domain/contract/controller/ContractController.java
@@ -2,11 +2,13 @@ package com.alioth.server.domain.contract.controller;
 
 import com.alioth.server.common.jwt.JwtTokenProvider;
 import com.alioth.server.common.response.CommonResponse;
+import com.alioth.server.domain.contract.dto.req.ContractCancellationDto;
 import com.alioth.server.domain.contract.dto.req.ContractCreateDto;
 import com.alioth.server.domain.contract.dto.req.ContractUpdateDto;
 import com.alioth.server.domain.contract.dto.res.ContractResDto;
 import com.alioth.server.domain.contract.service.ContractService;
 import jakarta.validation.Valid;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -27,6 +29,7 @@ public class ContractController {
     private final JwtTokenProvider  jwtTokenProvider;
 
 
+
     @PostMapping("/create")
     public ResponseEntity<CommonResponse> createContract( @AuthenticationPrincipal UserDetails userDetails, @RequestBody @Valid ContractCreateDto contractCreateDto) {
         ContractResDto createdContract = contractService.createContract(contractCreateDto, userDetails);
@@ -41,14 +44,21 @@ public class ContractController {
                 updatedContract
         );
     }
-    @DeleteMapping("/delete/{contractId}")
-    public ResponseEntity<CommonResponse> deleteContract(@PathVariable("contractId") Long contractId) {
-        contractService.deleteContract(contractId);
-        return CommonResponse.responseMessage(HttpStatus.OK, "계약이 성공적으로 삭제되었습니다.");
+    @PostMapping("/cancel/{contractId}")
+    public ResponseEntity<CommonResponse> cancelContract(@PathVariable("contractId") Long contractId, @RequestBody @Valid ContractCancellationDto cancellationDto) {
+        contractService.cancelContract(contractId, cancellationDto.reason());
+        return CommonResponse.responseMessage(HttpStatus.OK, "계약이 성공적으로 해지되었습니다.");
     }
+
     @GetMapping("/list")
     public ResponseEntity<CommonResponse> listAllContracts() {
-        List<ContractResDto> contracts = contractService.listAllContracts();
-        return CommonResponse.responseMessage(HttpStatus.OK, "모든 계약 목록을 성공적으로 조회했습니다.", contracts);
+        List<ContractResDto> contracts = contractService.listAllActiveContracts(); // 수정된 메소드 호출
+        return CommonResponse.responseMessage(HttpStatus.OK, "활성 계약 목록을 성공적으로 조회했습니다.", contracts);
+    }
+
+    @GetMapping("/detail/{contractId}")
+    public ResponseEntity<CommonResponse> getContractDetail(@PathVariable Long contractId) {
+        ContractResDto contractDetail = contractService.getContractDetails(contractId);
+        return CommonResponse.responseMessage(HttpStatus.OK, "계약 상세 정보를 성공적으로 조회했습니다.", contractDetail);
     }
 }
