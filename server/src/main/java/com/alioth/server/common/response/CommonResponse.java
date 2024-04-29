c5b789e6b55586ddcc3ffe3290c530c1239d5fe1
Mr.Jo <142702766+hoonjo123@users.noreply.github.com>
2024-04-29 11:49:45 +0900

    [fix]: 영업사원 RANK에 따라 건의사항 작성시 알람을 받는 범위 수정 및 hq직원도 건의사항 작성가능 (#90)

--- server/src/main/java/com/alioth/server/common/response/CommonResponse.java
+++ server/src/main/java/com/alioth/server/common/response/CommonResponse.java
@@ -1,6 +1,15 @@
 package com.alioth.server.common.response;
 
 import com.alioth.server.domain.answer.dto.res.AnswerResDto;
+import com.alioth.server.domain.board.dto.res.BoardResDto;
+import com.alioth.server.domain.contract.dto.res.ContractResDto;
+import com.alioth.server.domain.dummy.dto.res.SimpleListDTO;
+import com.alioth.server.domain.login.dto.res.LoginResDto;
+import com.alioth.server.domain.member.dto.res.SMTeamListResDto;
+import com.alioth.server.domain.member.dto.res.SalesMemberResDto;
+import com.alioth.server.domain.notification.dto.res.NotificationResDto;
+import com.alioth.server.domain.schedule.dto.req.ScheduleReqDto;
+import com.alioth.server.domain.team.dto.TeamResDto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.AllArgsConstructor;
@@ -16,7 +25,16 @@ public class CommonResponse {
     private String message;
 
     @Schema(description = "반환 오브젝트", anyOf = {
-            AnswerResDto.class
+            AnswerResDto.class,
+            BoardResDto.class,
+            ContractResDto.class,
+            SimpleListDTO.class,
+            LoginResDto.class,
+            SalesMemberResDto.class,
+            SMTeamListResDto.class,
+            NotificationResDto.class,
+            ScheduleReqDto.class,
+            TeamResDto.class
     })
     private Object result;
 
