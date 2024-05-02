de64be8043255537280f38a40127f103e4c78013
GyeongNam <63902992+GyeongNam@users.noreply.github.com>
2024-05-02 18:56:04 +0900

    Update Image.java

--- server/src/main/java/com/alioth/server/common/image/domain/Image.java
+++ server/src/main/java/com/alioth/server/common/image/domain/Image.java
@@ -5,14 +5,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Entity
 @Builder
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Image extends BaseEntity {
